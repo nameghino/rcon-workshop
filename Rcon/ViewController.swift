106e35ce3d768a72fa5f38a2e8187829e3c42698
Nico Ameghino <nicolas.ameghino@gmail.com>
2015-03-26 00:49:17 -0300

    Initial Commit

---
+++ Rcon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rcon
+//
+//  Created by Nicolas Ameghino on 3/26/15.
+//  Copyright (c) 2015 Nicolas Ameghino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
