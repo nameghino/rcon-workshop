c940dbf500eda832cac4d8a1fc302a92a7232092
Nicolas Ameghino <nicolas.ameghino@ywing.local>
2015-03-30 17:29:43 -0300

    Move commanding logic to the SparkService class

--- Rcon/SparkService.swift
+++ Rcon/SparkService.swift
@@ -8,12 +8,41 @@
 
 import UIKit
 
+extension NSData {
+    var stringRepresentation: String {
+        get {
+            return NSString(data: self, encoding: NSUTF8StringEncoding) as! String
+        }
+    }
+}
+
 let SharedSparkService = SparkService()
 let kAPIHost = "https://api.spark.io"
 let kAPIVersion = "v1"
 
 enum SparkServiceEndpoints {
     case CoreInformation(SparkCore)
+    case SetPin(SparkCore, Int, LogicLevel)
+    case TimedPin(SparkCore, Int, Int)
+    case TogglePin(SparkCore, Int)
+    case PinState(SparkCore)
+    
+    var core: SparkCore {
+        get {
+            switch self {
+            case let .SetPin(core, _, _):
+                return core
+            case let .TimedPin(core, _, _):
+                return core
+            case let .TogglePin(core, _):
+                return core
+            case let .PinState(core):
+                return core
+            case let .CoreInformation(core):
+                return core
+            }
+        }
+    }
     
     var URL: NSURL {
         get {
@@ -21,9 +50,70 @@ enum SparkServiceEndpoints {
             switch self {
             case .CoreInformation(let core):
                 endpoint = "devices/\(core.coreId)"
+            case .SetPin(let core, _, _):
+                endpoint = "/devices/\(core.coreId)/setPin"
+            case .TimedPin(let core, _, _):
+                endpoint = "/devices/\(core.coreId)/timedPin"
+            case .TogglePin(let core, _):
+                endpoint = "/devices/\(core.coreId)/togglePin"
+            case .PinState(let core):
+                endpoint = "/devices/\(core.coreId)/pinState"
             }
             let URLString = kAPIHost.stringByAppendingPathComponent(kAPIVersion).stringByAppendingPathComponent(endpoint)
             return NSURL(string: URLString)!
+        }
+    }
+    
+    var method: String {
+        get {
+            switch self {
+            case .PinState(_):
+                fallthrough
+            case .CoreInformation(_):
+                return "GET"
+            case .SetPin(_, _, _):
+                fallthrough
+            case .TimedPin(_, _, _):
+                fallthrough
+            case .TogglePin(_, _):
+                return "POST"
+            }
+        }
+    }
+    
+    var data: NSData? {
+        get {
+            let dict: JSONDictionary?
+            switch self {
+            case let .SetPin(_, pin, level):
+                dict = ["args": "D\(pin)@\(level.rawValue)"]
+            case let .TimedPin(_, pin, millis):
+                dict = ["args": "D\(pin)@\(millis)"]
+            case let .TogglePin(_, pin):
+                dict = ["args": "D\(pin)@1"]
+            default:
+                dict = nil
+            }
+            return NSJSONSerialization.dataWithJSONObject(dict!, options: .allZeros, error: nil)
+        }
+    }
+    
+    private func createBaseRequest(core: SparkCore) -> NSMutableURLRequest {
+        let r = NSMutableURLRequest(URL: self.URL)
+        r.setValue("Bearer \(core.authToken)", forHTTPHeaderField: "Authorization")
+        r.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        r.setValue("application/json", forHTTPHeaderField: "Accept")
+        return r
+    }
+    
+    var request: NSMutableURLRequest {
+        get {
+            let r = createBaseRequest(self.core)
+            r.HTTPMethod = self.method
+            if let d = self.data {
+                r.HTTPBody = d
+            }
+            return r
         }
     }
 }
@@ -36,27 +126,21 @@ class SparkService: NSObject {
         activeTasks = [:]
     }
     
-    private func createSparkAPIRequest(core: SparkCore, targetURL: NSURL) -> NSMutableURLRequest {
-        let r = NSMutableURLRequest(URL: targetURL)
-        r.setValue("Bearer \(core.authToken)", forHTTPHeaderField: "Authorization")
-        r.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        r.setValue("application/json", forHTTPHeaderField: "Accept")
-        return r
+    func submit(endpoint: SparkServiceEndpoints, callback: SparkCoreCommandCallback) -> String {
+        return self.submitRequest(endpoint.request, callback: callback)
     }
     
-    func createSparkCoreInformationRequest(core: SparkCore) -> NSMutableURLRequest {
-        return createSparkAPIRequest(core, targetURL: SparkServiceEndpoints.CoreInformation(core).URL)
-    }
-    
-    func submitRequest(request: NSURLRequest, callback: SparkCoreCommandCallback) -> String {
+    private func submitRequest(request: NSURLRequest, callback: SparkCoreCommandCallback) -> String {
         let session = NSURLSession.sharedSession()
         let task = session.dataTaskWithRequest(request) {
             (data, response, error) in
             if let cb = callback {
                 if error != nil {
+                    NSLog("error: \(error.localizedDescription)")
                     cb(error, [:])
                 } else {
-                    var jsonError: NSError?
+                    NSLog("response: \(data.stringRepresentation)")
+                        var jsonError: NSError?
                     if let jdict = NSJSONSerialization.JSONObjectWithData(data,
                         options: NSJSONReadingOptions.allZeros,
                         error: &jsonError) as? JSONDictionary {
