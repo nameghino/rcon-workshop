3ff3aa1ee5fbcacff9f94a3f0c861fc6c6d7aaf0
Nico Ameghino <nicolas.ameghino@gmail.com>
2015-04-07 15:30:05 -0300

    Add deleteCore methods for SparkCoreManager Add needsStatusUpdate() method for the SparkCore Add message, appliances and lastCloudStatusUpdate properties updateCloudState() method returns the task id for cancellation in the view controller

--- Rcon/SparkCore.swift
+++ Rcon/SparkCore.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Foundation
 
 typealias JSONDictionary = [String:AnyObject]
 typealias SparkCoreCommandCallback = ((NSError!, JSONDictionary) -> Void)?
@@ -28,6 +29,7 @@ enum SparkCoreStatus {
     case Unknown
     case Online
     case Offline
+    case Error
 }
 
 class SparkCore: NSObject, NSCoding {
@@ -44,6 +46,9 @@ class SparkCore: NSObject, NSCoding {
     var coreDescription: String
     var lastHeard: NSDate
     var pinState: [LogicLevel] = []
+    var message: String?
+    var appliances: [Appliance] = []
+    var lastCloudStatusUpdate: NSDate
     
     init(description: String, coreId: String, authToken: String) {
         self.coreId = coreId
@@ -51,6 +56,7 @@ class SparkCore: NSObject, NSCoding {
         self.state = .Unknown
         self.coreDescription = description
         self.lastHeard = NSDate.distantPast() as! NSDate
+        self.lastCloudStatusUpdate = NSDate.distantPast() as! NSDate
     }
     
     required init(coder aDecoder: NSCoder) {
@@ -59,6 +65,12 @@ class SparkCore: NSObject, NSCoding {
         self.lastHeard = aDecoder.decodeObjectForKey("lastHeard") as! NSDate
         self.authToken = aDecoder.decodeObjectForKey("authToken") as! String
         self.state = .Unknown
+        self.appliances = aDecoder.decodeObjectForKey("appliances") as! [Appliance]
+        self.lastCloudStatusUpdate = aDecoder.decodeObjectForKey("lastCloudStatusUpdate") as! NSDate
+        super.init()
+        for appliance in self.appliances {
+            appliance.core = self
+        }
     }
     
     func encodeWithCoder(aCoder: NSCoder) {
@@ -66,6 +78,8 @@ class SparkCore: NSObject, NSCoding {
         aCoder.encodeObject(self.coreDescription, forKey: "coreDescription")
         aCoder.encodeObject(self.lastHeard, forKey: "lastHeard")
         aCoder.encodeObject(self.authToken, forKey: "authToken")
+        aCoder.encodeObject(self.appliances, forKey: "appliances")
+        aCoder.encodeObject(self.lastCloudStatusUpdate, forKey: "lastCloudStatusUpdate")
     }
     
     
@@ -77,48 +91,69 @@ class SparkCore: NSObject, NSCoding {
     }
     
     /*
-     * Response
-
+    * Response
+    
     {
-        "id": "53ff75065075535155461187",
-        "name": "HOME",
-        "connected": true,
-        "variables": {
-            "pinState": "string"
-        },
-        "functions": [
-            "setPin",
-            "timedPin",
-            "togglePin"
-        ],
-            "cc3000_patch_version": "1.29",
-            "last_heard": "2015-03-28T15:38:01.142Z"
+    "id": "53ff75065075535155461187",
+    "name": "HOME",
+    "connected": true,
+    "variables": {
+    "pinState": "string"
+    },
+    "functions": [
+    "setPin",
+    "timedPin",
+    "togglePin"
+    ],
+    "cc3000_patch_version": "1.29",
+    "last_heard": "2015-03-28T15:38:01.142Z"
     }
     */
-
-
-    func updateCloudState(callback: SparkCoreCommandCallback) {
-        let request = SharedSparkService.submit(SparkServiceEndpoints.CoreInformation(self)) {
+    
+    
+    func needsStatusUpdate() -> Bool {
+        let interval = fabs(lastCloudStatusUpdate.timeIntervalSinceNow)
+        return interval > 60
+    }
+    
+    
+    func updateCloudState(callback: SparkCoreCommandCallback) -> String {
+        let taskId = SharedSparkService.submit(SparkServiceEndpoints.CoreInformation(self)) {
             [unowned self](error, info) -> Void in
-            if let name = info["name"] as? String {
-                self.coreDescription = name
-            }
-            if let connected = info["connected"] as? Bool {
-                if connected {
-                    self.state = .Online
+            
+            self.lastCloudStatusUpdate = NSDate()
+            
+            if let errorTitle = info["error"] as? String {
+                self.state = .Error
+                self.message = "\(errorTitle)"
+            } else {
+                
+                if let name = info["name"] as? String {
+                    self.coreDescription = name
+                }
+                if let connected = info["connected"] as? Bool {
+                    if connected {
+                        self.state = .Online
+                        self.lastHeard = NSDate()
+                    } else {
+                        self.state = .Offline
+                    }
                 } else {
-                    self.state = .Offline
+                    self.state = .Unknown
                 }
             }
             if let cb = callback {
                 cb(error, info)
             }
         }
-        
+        return taskId
     }
 }
 
 class SparkCoreManager {
+    
+    var cores: [SparkCore] = []
+    
     static let filename = "cores.archive"
     static var filepath: String {
         get {
@@ -130,9 +165,24 @@ class SparkCoreManager {
         load()
     }
     
-    var cores: [SparkCore] = []
     func addCore(description: String, coreId: String, authToken: String) {
         cores.append(SparkCore(description: description, coreId: coreId, authToken: authToken))
+        if !save() {
+            NSLog("error saving core table: \(strerror(errno))")
+        }
+    }
+    
+    func deleteCore(core: SparkCore) {
+        if let index = find(cores, core) {
+            cores.removeAtIndex(index)
+            if !save() {
+                NSLog("error saving core table: \(strerror(errno))")
+            }
+        }
+    }
+    
+    func deleteCore(index: Int) {
+        cores.removeAtIndex(index)
         if !save() {
             NSLog("error saving core table: \(strerror(errno))")
         }
@@ -143,6 +193,8 @@ class SparkCoreManager {
     }
     
     func load() {
+        NSLog("\(SparkCoreManager.filepath)")
+
         if let savedCores = NSKeyedUnarchiver.unarchiveObjectWithFile(SparkCoreManager.filepath) as? [SparkCore] {
             cores = savedCores
         }
